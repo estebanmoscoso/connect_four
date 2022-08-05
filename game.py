70a5263b60dcfead5825b89235daa950f9bde1ce
Sebastián Moscoso <esteban.moscoso@yachaytech.edu.ec>
2022-08-04 23:29:08 -0500

    Changed connect_4 method to check_connect_4

--- game.py
+++ game.py
@@ -52,35 +52,35 @@ class Game:
 
     def get_winner(self):
         for player in self.players:
-            if self.connect_4(player) >= 4:
+            if self.check_connect_4(player) >= 4:
                 return player
         return None
 
-    def connect_4(self, tag, i_s=0, j_s=0, direction=None, acc_tags=0):
+    def check_connect_4(self, tag, i_s=0, j_s=0, direction=None, acc_tags=0):
         result = 0
         if direction is None:
             for i in range(i_s, 7):
                 for j in range(j_s, 6):
                     if self.grid[j][i] == tag:
-                        down_result = self.connect_4(tag, i, j, 'down', acc_tags + 1)
-                        dl_result = self.connect_4(tag, i, j, 'down_left', acc_tags + 1)
-                        dr_result = self.connect_4(tag, i, j, 'down_right', acc_tags + 1)
-                        right_result = self.connect_4(tag, i, j, 'right', acc_tags + 1)
+                        down_result = self.check_connect_4(tag, i, j, 'down', acc_tags + 1)
+                        dl_result = self.check_connect_4(tag, i, j, 'down_left', acc_tags + 1)
+                        dr_result = self.check_connect_4(tag, i, j, 'down_right', acc_tags + 1)
+                        right_result = self.check_connect_4(tag, i, j, 'right', acc_tags + 1)
                         result = max(result, down_result, dr_result, dl_result, right_result)
             return result
         if direction == 'down':
             if j_s == 5 or self.grid[j_s + 1][i_s] != tag:
                 return acc_tags
-            return self.connect_4(tag, i_s, j_s + 1, 'down', acc_tags + 1)
+            return self.check_connect_4(tag, i_s, j_s + 1, 'down', acc_tags + 1)
         if direction == 'down_left':
             if j_s == 5 or i_s == 0 or self.grid[j_s + 1][i_s - 1] != tag:
                 return acc_tags
-            return self.connect_4(tag, i_s - 1, j_s + 1, 'down_left', acc_tags + 1)
+            return self.check_connect_4(tag, i_s - 1, j_s + 1, 'down_left', acc_tags + 1)
         if direction == 'down_right':
             if j_s == 5 or i_s == 6 or self.grid[j_s + 1][i_s + 1] != tag:
                 return acc_tags
-            return self.connect_4(tag, i_s + 1, j_s + 1, 'down_right', acc_tags + 1)
+            return self.check_connect_4(tag, i_s + 1, j_s + 1, 'down_right', acc_tags + 1)
         if direction == 'right':
             if i_s == 6 or self.grid[j_s][i_s + 1] != tag:
                 return acc_tags
-            return self.connect_4(tag, i_s + 1, j_s, 'right', acc_tags + 1)
+            return self.check_connect_4(tag, i_s + 1, j_s, 'right', acc_tags + 1)
